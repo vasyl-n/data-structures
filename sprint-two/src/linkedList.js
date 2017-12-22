64b3467b1262802c213dc5858167dfc9a3ebadd8
Vasyl Nesterenko <vasyl.nrk@gmail.com>
2017-12-21 18:47:02 -0800

    linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,30 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (list.head === null && list.tail === null ) {
+      list.head = newNode;
+      list.tail = newNode;
+    }
+    if( list.tail !== null ) {
+      list.tail.next = newNode;
+    }
+    list.tail = newNode;
   };
 
   list.removeHead = function() {
+    var headValue = list.head.value;
+    list.head = list.head.next;
+    return headValue;
   };
 
   list.contains = function(target) {
+    for ( var key in list ) {
+      if ( key.value === target ) {
+        return true;
+      }
+    }
+    return false;
   };
 
   return list;
