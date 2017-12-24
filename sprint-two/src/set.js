b9552428a20fcdf78bdb4066c522c52e6d6b2d72
vasyl-n <vasyl.nrk@gmail.com>
2017-12-24 12:01:49 -0800

    completes set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,21 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage.push(item);
 };
 
 setPrototype.contains = function(item) {
+  return this._storage.includes(item);
 };
 
 setPrototype.remove = function(item) {
+  this._storage.splice(this._storage.indexOf(item), 1);
 };
 
 /*
